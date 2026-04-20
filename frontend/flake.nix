1b46d35c8d23f42d112fea1e18044aa8354282ac
Ludvig <ludvig.dahlgren05@gmail.com>
2026-04-20 22:37:55 +0200

    Added flake.nix

---
+++ frontend/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Frontend for Catan in Haskell with Miso";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f:
+        builtins.listToAttrs (map (system: {
+          name = system;
+          value = f system;
+        }) systems);
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.haskell.compiler.ghc96
+              pkgs.cabal-install
+              pkgs.pkg-config
+              pkgs.zlib
+            ];
+
+            shellHook = ''
+              echo "Entered frontend dev shell"
+              ghc --version
+              cabal --version
+            '';
+          };
+        });
+    };
+}
