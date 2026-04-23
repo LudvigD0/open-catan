d1bf212bf0990518a4b238a5d5c350428d5bc2a0
Ludvig <ludvig.dahlgren05@gmail.com>
2026-04-24 00:31:34 +0200

    Added MakeFile, changed versions, fixed wasm so miso can be ran in browser

--- frontend/app/Main.hs
+++ frontend/app/Main.hs
@@ -1,4 +1,54 @@
+----------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE CPP               #-}
+----------------------------------------------------------------------------
 module Main where
-
+----------------------------------------------------------------------------
+import           Miso
+import qualified Miso.Html as H
+import qualified Miso.Html.Property as P
+import           Miso.Lens
+----------------------------------------------------------------------------
+-- | Sum type for App events
+data Action
+  = AddOne
+  | SubtractOne
+  | SayHelloWorld
+  deriving (Show, Eq)
+----------------------------------------------------------------------------
+-- | Entry point for a miso application
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = startApp defaultEvents app
+----------------------------------------------------------------------------
+-- | WASM export, required when compiling w/ the WASM backend.
+#ifdef WASM
+foreign export javascript "hs_start" main :: IO ()
+#endif
+----------------------------------------------------------------------------
+-- | `component` takes as arguments the initial model, update function, view function
+app :: App Int Action
+app = component 0 updateModel viewModel
+----------------------------------------------------------------------------
+-- | Updates model, optionally introduces side effects
+updateModel :: Action -> Effect parent Int Action
+updateModel = \case
+  AddOne -> this += 1
+  SubtractOne -> this -= 1
+  SayHelloWorld -> io_ $ do
+    alert "Hello World"
+    consoleLog "Hello World"
+----------------------------------------------------------------------------
+-- | Constructs a virtual DOM from a model
+viewModel :: Int -> View Int Action
+viewModel x =
+  H.div_
+    [ P.className "counter"
+    ]
+    [ H.button_ [ H.onClick AddOne ] [ text "+" ]
+    , text (ms x)
+    , H.button_ [ H.onClick SubtractOne ] [ text "-" ]
+    , H.br_ []
+    , H.button_ [ H.onClick SayHelloWorld ] [ text "Alert Hello World!" ]
+    ]
+----------------------------------------------------------------------------
--- frontend/flake.nix
+++ frontend/flake.nix
@@ -3,37 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    miso.url = "github:haskell-miso/miso";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = f:
-        builtins.listToAttrs (map (system: {
-          name = system;
-          value = f system;
-        }) systems);
-    in
-    {
-      devShells = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.haskell.compiler.ghc96
-              pkgs.cabal-install
-              pkgs.pkg-config
-              pkgs.zlib
-            ];
+  outputs = { self, nixpkgs, flake-utils, miso }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.haskell.compiler.ghc96
+            pkgs.cabal-install
+            pkgs.pkg-config
+            pkgs.zlib
+          ];
 
-            shellHook = ''
-              echo "Entered frontend dev shell"
-              ghc --version
-              cabal --version
-            '';
-          };
-        });
-    };
+          shellHook = ''
+            echo "Entered normal frontend dev shell"
+            ghc --version
+            cabal --version
+          '';
+        };
+
+        devShells.wasm = miso.devShells.${system}.wasm;
+      });
 }
