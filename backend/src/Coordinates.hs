d1658c71e67cd16043d7b258bc7b59862f6e5438
viktor <viktor.lehes@gmail.com>
2026-04-24 11:18:57 +0200

    board

---
+++ backend/src/Coordinates.hs
@@ -0,0 +1,91 @@
+module Coordinates where
+import Types
+import Data.Map()
+
+-- Every valid hex satisfies q + r + s = 0
+-- hexRange n generates all cube coords within radius n of the origin
+hexRange :: Int -> [Cord]
+hexRange n =
+    [ Cord q r (-q-r) | q <- [-n..n], r <- [max (-n) (-q-n) .. min n (-q+n)]
+    ]
+
+-- The 19 tile positions of a standard Catan board (radius 2)
+catanCords :: [Cord]
+catanCords = hexRange 2
+
+addCord :: Cord -> Cord -> Cord
+addCord (Cord q1 r1 s1) (Cord q2 r2 s2) = Cord (q1+q2) (r1+r2) (s1+s2)
+
+mkCord :: Int -> Int -> Int -> Maybe Cord
+mkCord q r s
+    | q + r + s == 0 = Just (Cord q r s)
+    | otherwise      = Nothing
+
+distance :: Cord -> Cord -> Int
+distance (Cord q1 r1 s1) (Cord q2 r2 s2) =
+    maximum [abs (q1-q2), abs (r1-r2), abs (s1-s2)]
+
+inBounds :: Cord -> Bool
+inBounds cord@(Cord q r s) = q + r + s == 0 && distance origin cord <= 2
+  where
+    origin = Cord 0 0 0
+
+-- All surrounding directions to a specific tile
+directions :: [Cord]
+directions =
+    [ Cord  1 (-1)  0
+    , Cord  1   0  (-1)
+    , Cord  0   1  (-1)
+    , Cord (-1)  1   0
+    , Cord (-1)  0   1
+    , Cord  0  (-1)  1
+    ]
+
+neighbours :: Cord -> [Cord]
+neighbours c = map (addCord c) directions
+
+-- Standard resource distribution
+-- fixed layout, reading from top-left to bottom-right
+
+tileData :: [(Maybe Resource, Int)]
+tileData =
+    -- 3 tiles, top
+    [ (Just Stone,   10)
+    , (Just Sheep,    2)
+    , (Just Wood,     9)
+    -- 4 tiles
+    , (Just Wheat,   12)
+    , (Just Clay,     6)
+    , (Just Sheep,    4)
+    , (Just Clay,    10)
+    -- 5 tiles, middle
+    , (Just Wheat,    9)
+    , (Just Wood,    11)
+    , (Nothing,       0)   -- Desert
+    , (Just Wood,     3)
+    , (Just Stone,    8)
+    -- 4 tiles
+    , (Just Wood,     8)
+    , (Just Stone,    3)
+    , (Just Wheat,    4)
+    , (Just Sheep,    5)
+    -- 3 tiles, bottom
+    , (Just Clay,     5)
+    , (Just Wheat,    6)
+    , (Just Sheep,   11)
+    ]
+
+makeTile :: Int -> (Maybe Resource, Int) -> (Cord, Tile)
+makeTile idx (res, tok) =
+    ( catanCords !! idx
+    , Tile
+        { tileId  = idx
+        , resource = res
+        , token    = tok
+        , robber   = res == Nothing   -- robber
+        , nodes    = []               
+        }
+    )
+
+catanTiles :: [(Cord, Tile)]
+catanTiles = zipWith makeTile [0..] tileData
